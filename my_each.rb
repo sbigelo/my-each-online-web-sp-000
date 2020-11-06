7d660011c51c432624bcd7a144b23b57657e9199
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-06 20:15:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,16 +1,10 @@
 def my_each(string)
- if block_given?
  count = 0
  while count < 4 
-  yield string[count]
+  yield string([count]
   
    count += 1
  
- end
- else empty_array = []
-   saved_block = Proc.new
-   my_each(empty_array) do |x|
-   "This block should not run!"
  end
  end
  string
