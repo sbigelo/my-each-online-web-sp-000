f63883f142749843e6c40d7e2d27923609696b92
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-06 20:16:54 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(string)
+ count = 0
+ while count < string.length 
+  yield(string[count])
+  
+   count += 1
+ 
+ end
+
+ string
 end
+
+
+# empty_array = []
+
+#     saved_block = Proc.new {
+#       my_each(empty_array) do |x|
+#         raise "This block should not run!"
+#       end
